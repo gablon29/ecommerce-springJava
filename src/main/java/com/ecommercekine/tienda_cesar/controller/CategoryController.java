12037e5c1e0226b685f83221e5c40e2dd1637b1c
gablon29 <gabriellondero40@gmail.com>
2025-02-17 19:09:29 -0300

    comenzando con los controladores de las categorias

--- src/main/java/com/ecommercekine/tienda_cesar/controller/CategoryController.java
+++ src/main/java/com/ecommercekine/tienda_cesar/controller/CategoryController.java
@@ -1,4 +1,32 @@
 package com.ecommercekine.tienda_cesar.controller;
 
+
+import com.ecommercekine.tienda_cesar.model.Category;
+import com.ecommercekine.tienda_cesar.response.ApiResponse;
+import com.ecommercekine.tienda_cesar.service.category.ICategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+    private final ICategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories(){
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Success", categories));
+        } catch (Exception exception) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error", INTERNAL_SERVER_ERROR));
+        }
+    }
+
 }
